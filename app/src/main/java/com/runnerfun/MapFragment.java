a1534a5034f7c6698ffc4c52efc572177b34b391
lixiaoyang <lixiaoyang02@meituan.com>
2016-10-18 21:26:25 +0800

    我也不记得这是啥了

--- app/src/main/java/com/runnerfun/MapFragment.java
+++ app/src/main/java/com/runnerfun/MapFragment.java
@@ -1,15 +1,21 @@
 package com.runnerfun;
 
 import android.os.Bundle;
-import android.support.annotation.MainThread;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.MapView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,9 +25,12 @@ import butterknife.OnClick;
  * Created by lixiaoyang on 16/10/2016.
  */
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements AMapLocationListener {
     @BindView(R.id.map)
     MapView mMap;
+
+    private AMapLocationClientOption mLocationOption = null;
+    private AMapLocationClient mlocationClient = null;
 
     @Nullable
     @Override
@@ -35,6 +44,13 @@ public class MapFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mMap.onCreate(savedInstanceState);
+        mlocationClient = new AMapLocationClient(getActivity());
+        mLocationOption = new AMapLocationClientOption();
+        mlocationClient.setLocationListener(this);
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setInterval(2000);
+        mlocationClient.setLocationOption(mLocationOption);
+        mlocationClient.startLocation();
     }
 
     @Override
@@ -62,4 +78,26 @@ public class MapFragment extends Fragment {
             ((MainFragment)f).switchToData();
         }
     }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (amapLocation != null) {
+            if (amapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                amapLocation.getLatitude();//获取纬度
+                amapLocation.getLongitude();//获取经度
+                amapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(amapLocation.getTime());
+                df.format(date);//定位时间
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError","location Error, ErrCode:"
+                        + amapLocation.getErrorCode() + ", errInfo:"
+                        + amapLocation.getErrorInfo());
+            }
+        }
+    }
+
 }
