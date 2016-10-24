1490d981ee7583458beab2f2db518628598d7f20
lixiaoyang <lixiaoyang02@meituan.com>
2016-10-24 22:40:30 +0800

    我也不记得这是啥了

--- app/src/main/java/com/runnerfun/RunFragment.java
+++ app/src/main/java/com/runnerfun/RunFragment.java
@@ -1,15 +1,15 @@
 package com.runnerfun;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,46 +20,62 @@ import butterknife.OnClick;
  */
 
 public class RunFragment extends Fragment {
-    @BindView(R.id.main_timer)
-    Chronometer mTimer;
-    @BindView(R.id.start)
-    Button mStartBtn;
+    @BindView(R.id.chronometer)
+    Chronometer mChronometer;
+    private TextView mKmValue;
+    private TextView mSpeedValue;
+    private TextView mKaclValue;
+    @BindView(R.id.money_value)
+    TextView mMoney;
 
     private boolean mIsRunning = false;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_run, container, false);
         ButterKnife.bind(this, rootView);
-
         return rootView;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mTimer.setFormat("%s");
-        mTimer.setBase(SystemClock.elapsedRealtime());
+        mChronometer.setFormat("11:44:77");
+        Typeface typeFace = Typeface.createFromAsset(getActivity().getAssets(),"fonts/dincond.otf");
+        Typeface boldTypeFace = Typeface.createFromAsset(getActivity().getAssets(),"fonts/dincond-bold.otf");
+        mChronometer.setTypeface(boldTypeFace);
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        mMoney.setTypeface(typeFace);
+        mMoney.setText("700");
+
+        View km = view.findViewById(R.id.km);
+        View speed = view.findViewById(R.id.speed);
+        View kacl = view.findViewById(R.id.kacl);
+
+        ((TextView) km.findViewById(R.id.en_title)).setTypeface(typeFace);
+        ((TextView) km.findViewById(R.id.value)).setTypeface(typeFace);
+        ((TextView) speed.findViewById(R.id.en_title)).setTypeface(typeFace);
+        ((TextView) speed.findViewById(R.id.value)).setTypeface(typeFace);
+        ((TextView) kacl.findViewById(R.id.en_title)).setTypeface(typeFace);
+        ((TextView) kacl.findViewById(R.id.value)).setTypeface(typeFace);
+
+        ((TextView) km.findViewById(R.id.en_title)).setText("km");
+        ((TextView) speed.findViewById(R.id.en_title)).setText("km/s");
+        ((TextView) kacl.findViewById(R.id.en_title)).setText("kacl");
+        ((TextView) km.findViewById(R.id.zh_title)).setText("距离");
+        ((TextView) speed.findViewById(R.id.zh_title)).setText("配速");
+        ((TextView) kacl.findViewById(R.id.zh_title)).setText("消耗");
+
+        mKmValue = (TextView) km.findViewById(R.id.value);
+        mSpeedValue = (TextView) speed.findViewById(R.id.value);
+        mKaclValue = (TextView) kacl.findViewById(R.id.value);
     }
 
-    @OnClick(R.id.start)
-    void onClickStart(){
-        if(!mIsRunning) {
-            mTimer.start();
-            mStartBtn.setText("停止");
-        }
-        else {
-            mTimer.stop();
-            mStartBtn.setText("开始");
-            mTimer.setBase(SystemClock.elapsedRealtime());
-        }
-        mIsRunning = !mIsRunning;
-    }
-
-    @OnClick(R.id.map)
-    void onMapClick(){
+    @OnClick(R.id.btn_run)
+    void start(){
         Fragment f = getParentFragment();
         if(f instanceof MainFragment){
             ((MainFragment)f).switchToMap();
         }
     }
+
 }
---
+++ app/src/main/java/com/runnerfun/widget/MapBtnWidget.java
@@ -0,0 +1,84 @@
+package com.runnerfun.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by lixiaoyang on 19/10/2016.
+ */
+
+public class MapBtnWidget extends LinearLayout {
+    public static interface OnButtonClick{
+        void onLeftClick();
+        void onRightClick();
+    }
+
+    private Paint mRedPaint;
+    private Paint mBlackPaint;
+    private Path mPath = new Path();
+    private OnButtonClick mListener;
+
+    public MapBtnWidget(Context context) {
+        super(context);
+        init();
+    }
+
+    public MapBtnWidget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MapBtnWidget(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void setOnClickListener(OnButtonClick listener){
+        mListener = listener;
+    }
+
+    private void init(){
+        setWillNotDraw(false);
+        mRedPaint = new Paint();
+        mRedPaint.setColor(Color.RED);
+        mBlackPaint = new Paint();
+        mBlackPaint.setColor(Color.BLACK);
+    }
+
+    @Override
+    protected void onDraw(Canvas c){
+        c.drawRect(0.f, 0.f, getWidth(), getHeight(), mRedPaint);
+        mPath.reset();
+        mPath.moveTo(0, 0);
+        mPath.lineTo(getWidth()/2 + (int)(getWidth() * 0.05) , 0);
+        mPath.lineTo(getWidth()/2 - (int)(getWidth() * 0.05), getHeight());
+        mPath.lineTo(0, getHeight());
+        mPath.close();
+        c.drawPath(mPath, mBlackPaint);
+        super.onDraw(c);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if(mListener == null){
+            return false;
+        }
+        float x = ev.getX();
+        float y = ev.getY();
+        float xRange =  (float)(getWidth()/2 * 1.1 - (y * (0.1 * getWidth()) / getHeight()));
+        if(x < xRange){
+            mListener.onLeftClick();
+        }
+        else {
+            mListener.onRightClick();
+        }
+        return true;
+    }
+}
