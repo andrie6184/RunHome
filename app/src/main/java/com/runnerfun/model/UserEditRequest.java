c86a6c30fe2e0c8209a5a34170c417e97c72f75b
andrie <andrie6184@gmail.com>
2016-11-08 17:36:27 +0800

    commit code

--- app/src/main/java/com/runnerfun/model/UserEditRequest.java
+++ app/src/main/java/com/runnerfun/model/UserEditRequest.java
@@ -1,10 +1,9 @@
 package com.runnerfun.model;
 
 import com.runnerfun.beans.ResponseBean;
-import com.runnerfun.beans.UserInfo;
 
 import retrofit2.http.POST;
-import retrofit2.http.Query;
+import retrofit2.http.Part;
 import rx.Observable;
 
 /**
@@ -14,11 +13,11 @@ import rx.Observable;
 
 public interface UserEditRequest {
     @POST("/user/modify")
-    Observable<ResponseBean<UserInfo>> register(@Query(value = "user_name") String user_name,
-                                                @Query(value = "age") int age,
-                                                @Query(value = "headimg", encoded = true) String headimg,
-                                                @Query(value = "headimg", encoded = true) String remarks,
-                                                @Query(value = "height") int height,
-                                                @Query(value = "sexy") String sexy
+    Observable<ResponseBean<String>> editUserInfo(@Part("user_name") String user_name,
+                                              @Part("age") int age,
+                                              @Part("headimg") String headimg,
+                                              @Part("remarks") String remarks,
+                                              @Part("height") int height,
+                                              @Part("sexy") String sexy
     );
 }
